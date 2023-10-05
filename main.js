7dfbed2ba528d831e8d23be336ac90cbcb58b681
Bruna Alves de Lima <103039837+brunaalvesss@users.noreply.github.com>
2023-10-05 09:28:58 -0300

    Add files via upload

--- main.js
+++ main.js
@@ -1,70 +1,12 @@
-function tocaSomPom(){
-    document.querySelector('#som_tecla_pom').play('');
+// Pom ----------------------------------------
+function tocaSomPom () {
+    document.querySelector('#som_tecla_pom').play();
 }
 
-const listaDeTeclas = document.querySelectorAll('.tecla')
+const listaDeTeclas = document.querySelectorAll('.tecla');
 
 listaDeTeclas[0].onclick = tocaSomPom;
 
 listaDeTeclas[1].onclick = tocaSomClap;
 listaDeTeclas[2].onclick = tocaSomTim;
 listaDeTeclas[3].onclick = tocaSomPuff;
-
-document.querySelector('.tecla_pom').onclick = toca
-
-
-document.querySelector('.tecla_pom').onclick = tocaSomPom ();
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
